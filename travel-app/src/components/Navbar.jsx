f02c2634b562af7fec0f4beb9b032fcb3d7fb526
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-06-15 21:11:02 +0530

    Navbar done with css

--- travel-app/src/components/Navbar.jsx
+++ travel-app/src/components/Navbar.jsx
@@ -54,15 +54,14 @@ function Navbar() {
                   <span className='ri-global-line'></span>
                   <p className='m-0'>English</p>
                 </div>
-                <div className='divider gradient-divider'>
-                  <a href="#" className='cartpage-cart-link position-relative'>
-                    <i className="bi bi-cart text-white fs-5"></i>
-                    <span className='cart-qount'>0</span>
-                  </a>
-                  <button className="btn sign-up btn-cutome text-white rounded-5 mx-3 px-3 py-2 fs-6 fw-semibold">
-                    Sign Up
-                  </button>
-                </div>
+                <div className='divider gradient-divider'></div>
+                <a href="#" className='cartpage-cart-link position-relative'>
+                  <i className="bi bi-cart text-white fs-5"></i>
+                  <span className='cart-count'>2</span>
+                </a>
+                <button className="btn sign-up btn-custome text-white rounded-5 mx-3 px-3 py-2 fs-6 fw-semibold">
+                  Sign Up
+                </button>
 
                 <button className='navbar-toggler nav-toggle d-block d-lg-none box-shadow-none'
                   type='button'
@@ -77,6 +76,48 @@ function Navbar() {
         </div>
       </div>
 
+      <div className='container'>
+        <div className='row py-0 py-lg-3 w-100 d-flex align-items-center'>
+          <div className='col-lg-9'>
+            <div className={`collapas navbar-collapse ${isMenuOpen} ? 'show' : "" `} id='navtoggle'>
+              <ul className='nav-menu list-unstyled m-0 d-flex flex-column flex-lg-row align-items-start align-items-lg-center gap-3 gap-xl-5 gap-lg-4'>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>Home</a>
+                </li>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>Tours</a>
+                </li>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>Hotels</a>
+                </li>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>Transports</a>
+                </li>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>Restaurants</a>
+                </li>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>About</a>
+                </li>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>News</a>
+                </li>
+                <li className='nav-items position-relative'>
+                  <a href="#" className='nav-link' onClick={closeMenu}>Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='col-lg-3'>
+            <div className="nav-input-box w-100 d-none d-lg-flex align-items-center justify-content-start gap-3">
+              <i className="bi bi-search"></i>
+              <input type="text" className='form-control form-control-sm w-100' placeholder='Destinations, Attraction'/>
+            </div>
+          </div>
+
+        </div>
+      </div>
       
     </nav>
   )
